65ff0b10e4b64593694e81fb45e05ad05be4ac38
FPNMB <FPNMB@Zab_38>
2013-02-24 16:54:03 -0500

    new update

--- NXT_Lejos_Examples/src/TestIRSensor.java
+++ NXT_Lejos_Examples/src/TestIRSensor.java
@@ -1,12 +1,17 @@
+import javax.microedition.lcdui.Screen;
+
 import com.josh.lejos.util.BaseLejos;
 
+import lejos.nxt.LCD;
 import lejos.nxt.SensorPort;
 import lejos.nxt.addon.OpticalDistanceSensor;
 
 public class TestIRSensor extends BaseLejos {
 
 	 OpticalDistanceSensor sensor = new OpticalDistanceSensor(SensorPort.S1);
-	public int getdistance() {
+	 int distance = 0;
+	 int x = 0;
+	 public int getdistance() {
 	
 		return sensor.getDistance();
 
@@ -14,15 +19,50 @@ public class TestIRSensor extends BaseLejos {
 
 	public void display() {
 
-		int distance = getdistance();
-		System.out.println(distance);
+		distance = getdistance();
+		
 	}
+	
+	
 
 	@Override
 	public void run() {
 		while (true) {
+			clearscreen();
 			display();
+			wave();
 		}
+	}
+	
+	public void clearscreen() 
+	{
+	if (x > 100)
+	{
+		LCD.clear();
+		x = 0;
+	}	;
+	
+	
+	}
+	
+	public void wave() 
+	{
+
+		int y = 64-distance/15;
+//		LCD.drawString("x",distance/150, vertical);
+		LCD.setPixel(x,y, 1);
+		LCD.setPixel(x+1,y,  1);
+		LCD.setPixel(x, y+1, 1);
+		LCD.setPixel( x+1, y+1,1);
+		x = x + 1;
+	
+	try {
+		Thread.sleep(100);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
 	}
 
 	public static void main(String[] args)
