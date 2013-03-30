fe63ff9cdf1730788513b653618ef6a544c07645
Joshua Kim <joshua.kim.snailman@gmail.com>
2013-03-29 22:28:30 -0400

    new tyhing

--- SciFairNXJClient/src/org/scifair/controller/Controller.java
+++ SciFairNXJClient/src/org/scifair/controller/Controller.java
@@ -11,16 +11,20 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 import javax.swing.Timer;
 
+import lejos.nxt.LCD;
+
 import org.lejos.pcsample.usbsend.DrawPanel;
 import org.scifair.controller.NXTCommunications.Msg;
 
-public class Controller implements ActionListener{
+public class Controller implements ActionListener {
 	public static Timer timer = null;
-	private int x;
-	int line = 0;
+	private int xOld;
+	private int yOld = xOld;
+	int line = xOld;
 	int afterline = 0;
+    int level = 0;
+	DrawPanel drawPanel = null;
 
-	DrawPanel  drawPanel = null;
 	public DrawPanel getDrawPanel() {
 		return drawPanel;
 	}
@@ -28,50 +32,70 @@ public class Controller implements ActionListener{
 	public void setDrawPanel(DrawPanel drawPanel) {
 		this.drawPanel = drawPanel;
 	}
- 
+
 	private static Controller controller = null;
 
-	private Controller()
-	{
+	private Controller() {
 		timer = new Timer(50, this);
 	}
-	
-	public static Controller singleton()
-	{
-		if(controller ==null)
-		{
+
+	public static Controller singleton() {
+		if (controller == null) {
 			controller = new Controller();
-		}		
-		
-		return controller;
+		}
+
+		return controller; 
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(drawPanel==null)return;
-		
+		if (drawPanel == null)
+			return;
+
 		InputStream is = getClass().getResourceAsStream("poisonous_potato.png");
 		BufferedImage img = null;
 		try {
-		    img = ImageIO.read(is);
+			img = ImageIO.read(is);
 		} catch (IOException ex) {
 		}
-		
-		//img has the image data, now draw it on the scrween
+
+		// img has the image data, now draw it on the scrween
 		Graphics g = drawPanel.getGraphics();
 		NXTCommunications comm = NXTCommunications.singleton();
 		comm.sendMsg('d', 0);
-		Msg msg = comm.rcvMsg();
 		
-		if(msg!=null)
-		{
-			//here is the drawing code
-			
-			g.draw3DRect(x++,(int)msg.value, 1, 1, true);
-//			g.drawImage(img, x++, (int)msg.value, 300, 300, Color.white, null);
+		Msg msg = comm.rcvMsg();
+
+		int xNew, yNew;
+
+		if (msg != null) {
+			// here is the drawing code
+
+			xNew = xOld + 8;
+			yNew = (int) msg.value / 10 + level ;
+
+			g.draw3DRect(xNew, yNew, 1, 1, true);
+			g.drawLine(xOld, yOld, xNew, yNew);
+
+			System.out.println(xNew);
+			if (xNew >= drawPanel.getWidth()) {
+				level = level + 250;
+				if (level == 1000) {
+					g.setColor(Color.white);
+					g.fillRect(0, 0, drawPanel.getWidth(), drawPanel.getHeight());	
+		    level = 0;		
+				}
+				
+				
+
+				xOld = 0;
+				xNew = 0;
+			} else {
+				xOld = xNew;
+				yOld = yNew;
+			}
 		}
 
-		
 	}
-	
+
 }
