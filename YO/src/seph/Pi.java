ed8766fdc83739e8f94efaf217645400fd8b38cf
FPNMB <FPNMB@Zab_38>
2013-09-02 12:58:24 -0400

    derp

--- YO/src/seph/Pi.java
+++ YO/src/seph/Pi.java
@@ -2,28 +2,39 @@ package seph;
 
 public class Pi {
 
-	double sum  = 0;
 	double denominator = 1;
 	double sign = 1;
-	
-	
-	public void myfunction()
-	
-	{
-		
+    double both = 0;
+	public double flipSign() {
+		sign = sign * -1.0;
+		return sign;
+	}
+
+	public double nextTerm() {
+		double term = 1.0 / denominator * sign;
+		flipSign();
+		denominator = denominator + 2;
+		both = both + term;
+		return term;
 	}
 	
+	public void answer(){
+		both = both * 4;
+		System.out.println(both);
+	}
 
 	public static void main(String[] args) {
-		
+
 		Pi pi = new Pi();
-		
-		for(int i  = 0; i < 100000; i++)
-		{
-		
+
+		for (int i = 0; i < 1000000; i++) {
+
+			System.out.println(pi.nextTerm());
+			
 		}
+		pi.answer();
+		
+
 	}
-	
-	
-	
+
 }
