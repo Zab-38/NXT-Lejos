66ef75d3d4aa07d81efcb977effe7b7fbba8f5b5
FPNMB <FPNMB@Zab_38>
2013-09-02 16:23:38 -0400

    Yeha

--- YO/src/seph/Eulers.java
+++ YO/src/seph/Eulers.java
@@ -1,44 +1,32 @@
 package seph;
 
 public class Eulers {
-	double denominator = 1;
-	double increase = 1;
-	double answer = 0;
-	public void change() {
-		denominator = denominator * increase;
-		increase = increase + 1;
+	int denominator = 1;
+	int increase = 1;
+	
+	public void changedenominator() {
+	    denominator = denominator * increase;
+	    increase = increase + 1;
+	}
+	//WHAT IS WRONG HEEEREEE :( (not just there but you will see what i mean when it is run :D)
+	public int count() {
+		int count = 0;
+		count = count + 1;
+		return count;
 	}
 	
 	public void print() {
 		System.out.println(denominator);
-	}
-	
-	public void add() {
+	} 
+	public static void main(String[] args) {
+		Eulers e = new Eulers();
 		
-//		 1 + 1/1! + 1/2! + 1/3! + 1/4! + 1/5! + 1/6! + 1/7! +
-//		 in other words: answer + 1/denominator 
-
-//		IT IS NOT  1+ 1! + 2! + 3! 
-		answer = answer + denominator;
-	}
-	
-	public void answer() {
+		while (e.count() < 100);
+		{
+			e.changedenominator();
+			e.print();
+			e.count();
+		}
 		
-//		  this is incorrect, answer is simply the sum of all the terms
-		answer = answer / 10;
-		System.out.println(answer);
 	}
-	
-	
-public static void main(String[] args) {
-	Eulers e = new Eulers();
-	for(int i  = 0; i < 10; i++)
-	{
-	e.print();
-	e.change();
-	e.add();
-	}
-	e.answer();
-	
-}
 }
