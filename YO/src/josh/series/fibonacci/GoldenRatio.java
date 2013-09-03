bd5290a218d19d00ae46df65487ab6aa42482d75
docere <docere@gmail.com>
2013-09-03 08:28:23 -0400

    added Golden Ratio problem

---
+++ YO/src/josh/series/fibonacci/GoldenRatio.java
@@ -0,0 +1,12 @@
+package josh.series.fibonacci;
+
+/*
+  http://www.mathsisfun.com/numbers/nature-golden-ratio-fibonacci.html
+ *
+ * 
+ * Approximate the golden ratio using the fibonacci sequence
+ * 
+ */
+public class GoldenRatio {
+
+}
