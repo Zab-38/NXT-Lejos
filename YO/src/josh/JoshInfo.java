49155791379e291d141178888bde0ba4e6fbb981
docere <docere@Toshiba-Ultra>
2013-09-03 12:03:55 -0400

    bussing information for Josh

--- YO/src/josh/JoshInfo.java
+++ YO/src/josh/JoshInfo.java
@@ -4,6 +4,30 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+/**
+ * bussing information:
+ * 
+ * Leaving From Trowbridge to Churchill:
+ * be there 8:07.  take the North point 2 bus to hammarskjold, tell the bus driver that you will be transferring at hammarskjold
+ * (will stop at superior first).  At Hammarskjold transfer to north 3 shuttle to Churchill. 
+ * 
+ * Leaving from Churchill to Trowbridge:
+ * get to bus as soon as school is out.  Take North 23 to Hammarskjold. Transfer to North 20 and arrive home 16:39
+ * 
+ * 
+ * 
+ * 
+ * Leaving from Moms to Churchill:
+ * Walk to superior, get there for 9:00.  catch the North 16 to hamarskjold, take north 3 shuttle to churchill
+ * 
+ * Leaving Churchill to Moms:
+ * North 23 to Hammarskjold to take North 16 to superior and then walk home
+ * 
+ *  
+ * @author docere
+ *
+ */
+
 public class JoshInfo {
 
 	public static final String bussingWebsite = "http://www.ststb.ca/";
