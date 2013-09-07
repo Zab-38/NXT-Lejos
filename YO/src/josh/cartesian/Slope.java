f066c51c3f620acd9eb7637a825728d641f60ad1
docere <docere@gmail.com>
2013-09-07 18:20:21 -0400

    cleaned up Slope

--- YO/src/josh/cartesian/Slope.java
+++ YO/src/josh/cartesian/Slope.java
@@ -3,12 +3,6 @@ package josh.cartesian;
 public class Slope {
   
 	
-	double x1 = 1;
-  double y1 = 2;
-  double x2 = 3;
-  double y2 = 4;
-  double ydouble = y2 - y1;
-  double xdouble = x2 - x1;
   public double calculate(Point point1, Point point2) {
 	  
 	  double slope = (point2.y - point1.y) / (point2.x - point1.x);
